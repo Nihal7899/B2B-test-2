67ba6cadc6bbd3822b9bdd7a2cb0b0827a0ab5f1
Nihal7899 <abdulraheemnihal2@gmail.com>
2026-09-16 02:17:39 +0530

    ghh

--- src/components/AppLoader.tsx
+++ src/components/AppLoader.tsx
@@ -202,7 +202,7 @@ export const AppLoader = React.memo(function AppLoader({
                   1. Reduced by 5% (scale 2.66 down from 2.8)
                   2. Safely translated left by 25px and down by 20px (-300 - 25 = -325, -980 + 20 = -960)
                 */}
-                <g transform="translate(-360, -940) scale(2.66)">
+                <g transform="translate(-360, -920) scale(2.66)">
                   <g className="animate-produce-jiggle">
                     {/* Lettuce */}
                     <circle cx="280" cy="350" r="35" fill="url(#lettuceGrad1)" />
